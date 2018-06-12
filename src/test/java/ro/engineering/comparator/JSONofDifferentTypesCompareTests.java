69fc9403cb4afc4c501bf6b3678f15a6aae9ab98
Florin Slevoaca <florin.slevoaca@1and1.ro>
2018-06-12 18:39:17 +0300

    More test scenarios

---
+++ src/test/java/ro/engineering/comparator/JSONofDifferentTypesCompareTests.java
@@ -0,0 +1,20 @@
+package ro.engineering.comparator;
+
+import org.junit.Test;
+
+public class JSONofDifferentTypesCompareTests {
+
+    @Test
+    public void compareObjectWithArray() {
+        String expected = "{\"a\":\"!test\"}";
+        String actual = "[\"a\",\"b\"]";
+        JSONCompare.assertNotEquals(expected, actual);
+    }
+
+    @Test
+    public void compareArrayWithObject() {
+        String expected = "[\"a\",\"b\"]";
+        String actual = "{\"a\":\"!test\"}";
+        JSONCompare.assertNotEquals(expected, actual);
+    }
+}
