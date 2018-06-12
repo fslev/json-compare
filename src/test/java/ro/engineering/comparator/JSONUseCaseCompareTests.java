3406461df922a716eea2c471e3643cd2dcd61a32
Florin Slevoaca <florin.slevoaca@1and1.ro>
2018-06-12 14:20:20 +0300

    More test scenarios

--- src/test/java/ro/engineering/comparator/JSONUseCaseCompareTests.java
+++ src/test/java/ro/engineering/comparator/JSONUseCaseCompareTests.java
@@ -5,6 +5,34 @@ import org.junit.Test;
 public class JSONUseCaseCompareTests {
 
     @Test
+    public void compareViaDoNotFindUseCaseOnValue() {
+        String expected = "{\"a\":\"!test\"}";
+        String actual = "{\"a\":\"testing\"}";
+        JSONCompare.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void compareViaDoNotFindUseCaseOn_negative() {
+        String expected = "{\"a\":\"!test\"}";
+        String actual = "{\"a\":\"test\"}";
+        JSONCompare.assertNotEquals(expected, actual);
+    }
+
+    @Test
+    public void compareViaDoNotFindUseCaseOnField() {
+        String expected = "{\"!a\":\"value does not matter\"}";
+        String actual = "{\"ab\":\"value does not matter\"}";
+        JSONCompare.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void compareViaDoNotFindUseCaseOnField_negative() {
+        String expected = "{\"!a\":\"value does not matter\"}";
+        String actual = "{\"a\":\"value does not matter\"}";
+        JSONCompare.assertNotEquals(expected, actual);
+    }
+
+    @Test
     public void compareLiteral() {
         String expected = "{\"a\":\"\\\\!some \\\\\\\\!text\"}";
         String actual = "{\"a\":\"!some \\\\!text\"}";
