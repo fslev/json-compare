ddfc4cd9be3c6856e86a831973833d2b52e086fe
Florin <slevoaca.florin@gmail.com>
2018-07-15 01:10:50 +0300

    Use case insensitive by default

--- src/main/java/ro/skyah/comparator/DefaultJsonComparator.java
+++ src/main/java/ro/skyah/comparator/DefaultJsonComparator.java
@@ -1,12 +1,25 @@
 package ro.skyah.comparator;
 
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
 public class DefaultJsonComparator implements JsonComparator {
 
     public boolean compareValues(Object expected, Object actual) {
-        return false;
+        try {
+            Pattern pattern = Pattern.compile(expected.toString(), Pattern.CASE_INSENSITIVE | Pattern.DOTALL | Pattern.MULTILINE);
+            return pattern.matcher(actual.toString()).matches();
+        } catch (PatternSyntaxException e) {
+            return expected.toString().toLowerCase().equals(actual.toString().toLowerCase());
+        }
     }
 
     public boolean compareFields(String expected, String actual) {
-        return false;
+        try {
+            Pattern pattern = Pattern.compile(expected, Pattern.CASE_INSENSITIVE | Pattern.DOTALL | Pattern.MULTILINE);
+            return pattern.matcher(actual).matches();
+        } catch (PatternSyntaxException e) {
+            return expected.toLowerCase().equals(actual.toLowerCase());
+        }
     }
 }
