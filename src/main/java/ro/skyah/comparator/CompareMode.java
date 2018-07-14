f15a68a24d8aabe03a7bd4926d2208d54d0ad399
Florin <slevoaca.florin@gmail.com>
2018-07-14 23:44:14 +0300

    Remove case insensitive and do not use regex modes

--- src/main/java/ro/skyah/comparator/CompareMode.java
+++ src/main/java/ro/skyah/comparator/CompareMode.java
@@ -3,7 +3,5 @@ package ro.skyah.comparator;
 public enum CompareMode {
     JSON_OBJECT_NON_EXTENSIBLE,
     JSON_ARRAY_NON_EXTENSIBLE,
-    JSON_ARRAY_STRICT_ORDER,
-    DO_NOT_USE_REGEX,
-    CASE_INSENSITIVE
+    JSON_ARRAY_STRICT_ORDER
 }
