c9f0bee1462500e96c6ac40c1c3ef639c916895b
Florin Slevoaca <florin.slevoaca@1and1.ro>
2018-07-17 18:54:13 +0300

    Add more methods to compare jsons

--- src/main/java/ro/skyah/comparator/JSONCompare.java
+++ src/main/java/ro/skyah/comparator/JSONCompare.java
@@ -21,88 +21,110 @@ public class JSONCompare {
         assertEquals(null, expected, actual, compareModes);
     }
 
-    public static void assertEquals(String expected, String actual, JsonComparator comparator, CompareMode... compareModes) {
+    public static void assertEquals(String expected, String actual, JsonComparator comparator,
+            CompareMode... compareModes) {
         assertEquals(null, expected, actual, comparator, compareModes);
     }
 
     public static void assertNotEquals(String expected, String actual,
-                                       CompareMode... compareModes) {
+            CompareMode... compareModes) {
         assertNotEquals(null, expected, actual, compareModes);
     }
 
     public static void assertNotEquals(String expected, String actual, JsonComparator comparator,
-                                       CompareMode... compareModes) {
+            CompareMode... compareModes) {
         assertNotEquals(null, expected, actual, comparator, compareModes);
     }
 
     public static void assertEquals(String message, String expected, String actual,
-                                    CompareMode... compareModes) {
+            CompareMode... compareModes) {
         JsonNode expectedJson = getJson(expected);
         JsonNode actualJson = getJson(actual);
         assertEquals(message, expectedJson, actualJson, null, compareModes);
     }
 
-    public static void assertEquals(String message, String expected, String actual, JsonComparator comparator,
-                                    CompareMode... compareModes) {
+    public static void assertEquals(String message, String expected, String actual,
+            JsonComparator comparator, CompareMode... compareModes) {
         JsonNode expectedJson = getJson(expected);
         JsonNode actualJson = getJson(actual);
         assertEquals(message, expectedJson, actualJson, comparator, compareModes);
     }
 
     public static void assertEquals(String message, String expected, JsonNode actual,
-                                    CompareMode... compareModes) {
+            CompareMode... compareModes) {
         JsonNode expectedJson = getJson(expected);
         assertEquals(message, expectedJson, actual, null, compareModes);
     }
 
-    public static void assertEquals(String message, String expected, JsonComparator comparator, JsonNode actual,
-                                    CompareMode... compareModes) {
+    public static void assertEquals(String message, String expected, JsonComparator comparator,
+            JsonNode actual, CompareMode... compareModes) {
         JsonNode expectedJson = getJson(expected);
         assertEquals(message, expectedJson, actual, comparator, compareModes);
     }
 
     public static void assertNotEquals(String message, String expected, JsonNode actual,
-                                       CompareMode... compareModes) {
+            CompareMode... compareModes) {
         JsonNode expectedJson = getJson(expected);
         assertNotEquals(message, expectedJson, actual, null, compareModes);
     }
 
-    public static void assertNotEquals(String message, String expected, JsonNode actual, JsonComparator comparator,
-                                       CompareMode... compareModes) {
+    public static void assertNotEquals(String message, String expected, JsonNode actual,
+            JsonComparator comparator, CompareMode... compareModes) {
         JsonNode expectedJson = getJson(expected);
         assertNotEquals(message, expectedJson, actual, comparator, compareModes);
     }
 
     public static void assertNotEquals(String message, String expected, String actual,
-                                       CompareMode... compareModes) {
+            CompareMode... compareModes) {
         JsonNode expectedJson = getJson(expected);
         JsonNode actualJson = getJson(actual);
         assertNotEquals(message, expectedJson, actualJson, null, compareModes);
     }
 
     public static void assertNotEquals(String message, String expected, String actual,
-                                       JsonComparator comparator, CompareMode... compareModes) {
+            JsonComparator comparator, CompareMode... compareModes) {
         JsonNode expectedJson = getJson(expected);
         JsonNode actualJson = getJson(actual);
         assertNotEquals(message, expectedJson, actualJson, comparator, compareModes);
     }
 
-    public static void assertEquals(String message, JsonNode expected, JsonNode actual, JsonComparator comparator,
-                                    CompareMode... compareModes) {
+    public static void assertEquals(JsonNode expected, JsonNode actual,
+            CompareMode... compareModes) {
+        assertEquals(null, expected, actual, null, compareModes);
+    }
+
+    public static void assertNotEquals(JsonNode expected, JsonNode actual,
+            CompareMode... compareModes) {
+        assertNotEquals(null, expected, actual, null, compareModes);
+    }
+
+    public static void assertEquals(JsonNode expected, JsonNode actual, JsonComparator comparator,
+            CompareMode... compareModes) {
+        assertEquals(null, expected, actual, comparator, compareModes);
+    }
+
+    public static void assertNotEquals(JsonNode expected, JsonNode actual,
+            JsonComparator comparator, CompareMode... compareModes) {
+        assertNotEquals(null, expected, actual, comparator, compareModes);
+    }
+
+    public static void assertEquals(String message, JsonNode expected, JsonNode actual,
+            JsonComparator comparator, CompareMode... compareModes) {
         try {
-            new JsonMatcher(expected, actual, comparator == null ? new DefaultJsonComparator() : comparator, new HashSet<CompareMode>(Arrays.asList(compareModes)))
-                    .matches();
+            new JsonMatcher(expected, actual,
+                    comparator == null ? new DefaultJsonComparator() : comparator,
+                    new HashSet<CompareMode>(Arrays.asList(compareModes))).matches();
         } catch (MatcherException e) {
             fail(message == null ? String.format("%s\nExpected:\n%s\nBut got:\n%s ", e.getMessage(),
-                    prettyPrint(expected),
-                    prettyPrint(actual)) : message);
+                    prettyPrint(expected), prettyPrint(actual)) : message);
         }
     }
 
-    public static void assertNotEquals(String message, JsonNode expectedJson, JsonNode actualJson, JsonComparator comparator,
-                                       CompareMode... compareModes) {
+    public static void assertNotEquals(String message, JsonNode expectedJson, JsonNode actualJson,
+            JsonComparator comparator, CompareMode... compareModes) {
         try {
-            new JsonMatcher(expectedJson, actualJson, comparator == null ? new DefaultJsonComparator() : comparator,
+            new JsonMatcher(expectedJson, actualJson,
+                    comparator == null ? new DefaultJsonComparator() : comparator,
                     new HashSet<CompareMode>(Arrays.asList(compareModes))).matches();
         } catch (MatcherException e) {
             return;
