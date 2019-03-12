ef8d7c11306d76f273b9b69111c1665c36273f4f
Florin Slevoaca <florin.slevoaca@ionos.com>
2019-03-12 18:54:39 +0200

    more tests

---
+++ src/test/java/ro/skyah/comparator/issues/Issue6Test.java
@@ -0,0 +1,46 @@
+package ro.skyah.comparator.issues;
+
+import org.junit.Test;
+import ro.skyah.comparator.JSONCompare;
+
+public class Issue6Test {
+
+    @Test
+    public void testIssue1() {
+        String expected = "[" +
+                "{\n" +
+                "  \"name\" : \"division1\",\n" +
+                "  \"vlan\" : \"116\"\n" +
+                "}," +
+                "{\n" +
+                "  \"!name\" : \"division1\",\n" +
+                "  \"!vlan\" : \"115\"\n" +
+                "}]";
+        String actual = "[{\n" +
+                "  \"name\" : \"division1\",\n" +
+                "  \"vlan\" : \"116\"\n" +
+                "}]";
+        JSONCompare.assertEquals(expected, actual);
+    }
+
+    @Test(expected = AssertionError.class)
+    public void testIssue1_negative() {
+        String expected = "[{\n" +
+                "  \"name\" : \"division1\",\n" +
+                "  \"vlan\" : \"116\"\n" +
+                "}," +
+                "{\n" +
+                "  \"!name\" : \"division1\",\n" +
+                "  \"!vlan\" : \"115\"\n" +
+                "}]";
+        String actual = "[{\n" +
+                "  \"name\" : \"division1\",\n" +
+                "  \"vlan\" : \"116\"\n" +
+                "}," +
+                "{\n" +
+                "  \"name\" : \"division1\",\n" +
+                "  \"vlan\" : \"115\"\n" +
+                "}]";
+        JSONCompare.assertEquals(expected, actual);
+    }
+}
