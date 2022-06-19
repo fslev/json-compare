047c70f431df0d3ef8e770c4572cb45a30498c8a
Florin Slevoaca <florin.slevoaca@ionos.com>
2022-06-19 15:09:44 +0300

    add extra checks

--- src/test/java/ro/skyah/comparator/JSONCompareMessageTests.java
+++ src/test/java/ro/skyah/comparator/JSONCompareMessageTests.java
@@ -15,8 +15,21 @@ public class JSONCompareMessageTests {
         String actual = "{\"ab\":true}";
         try {
             JSONCompare.assertMatches(expected, actual, new HashSet<>(Arrays.asList(CompareMode.JSON_OBJECT_NON_EXTENSIBLE)), "JSONs are not equal");
+            JSONCompare.assertMatches(expected, actual, null, new HashSet<>(Arrays.asList(CompareMode.JSON_OBJECT_NON_EXTENSIBLE)), "JSONs are not equal");
         } catch (AssertionError e) {
             assertTrue(e.getMessage().contains("JSONs are not equal"));
+        }
+    }
+
+    @Test
+    public void checkMessageFromJSONObjectFailedCompare_negative() {
+        String expected = "{\"a\":true,\"b\":0}";
+        String actual = "{\"a\":true,\"b\":0}";
+        try {
+            JSONCompare.assertNotMatches(expected, actual, new HashSet<>(Arrays.asList(CompareMode.JSON_OBJECT_NON_EXTENSIBLE)), "JSONs are not equal");
+            JSONCompare.assertNotMatches(expected, actual, null, new HashSet<>(Arrays.asList(CompareMode.JSON_OBJECT_NON_EXTENSIBLE)), "JSONs are not equal");
+        } catch (AssertionError e) {
+            assertTrue(e.getMessage().contains("JSONs are equal"));
         }
     }
 
